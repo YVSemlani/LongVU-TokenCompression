747d49f26cd6ef5c2ada8bfc26b65a36800ee08d
Yash V Semlani <yvs@oprime.cs.unc.edu>
2025-01-29 13:40:20 -0500

    MVBench evaluation complete. Paper accuracy replicated with LongVU Qwen 7B

--- slurm/eval_mvbench.sh
+++ slurm/eval_mvbench.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #SBATCH --job-name=longvu_eval_mvbench
 #SBATCH --output=/mnt/meg/yvs/LongVU/logs/eval_mvbench_%j.out
 #SBATCH --error=/mnt/meg/yvs/LongVU/logs/eval_mvbench_%j.err
@@ -6,7 +7,7 @@
 
 cd /mnt/meg/yvs/LongVU
 
-#pip install -r requirements.txt
+pip install -r requirements.txt
 
 #echo "Checking transformers version:"
 #pip3 show transformers | grep Version
@@ -15,7 +16,11 @@ cd /mnt/meg/yvs/LongVU
 #echo "Checking active conda environment:"
 #conda info
 
-num_gpus=1
-torchrun --standalone --nnodes 1 --nproc_per_node $num_gpus eval/eval_mvbench.py --data_path ./data/MVBench --version qwen --model_path checkpoints/longvu_qwen
+export NUM_GPUS=$SLURM_GPUS
+echo "NUM_GPUS: $NUM_GPUS"
+# Add environment variable to control memory allocation strategy
+export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:512
+# Add environment variable to enable work distribution across GPUs
+#export CUDA_VISIBLE_DEVICES=0,1
+torchrun --standalone --nnodes 1 --nproc_per_node $NUM_GPUS eval/eval_mvbench.py --data_path ./data/MVBench --version qwen --model_path checkpoints/longvu_qwen
 #torchrun --nproc-per-node=8 eval/eval_mvbench.py --data_path ./data/MVBench --version qwen --model_path ./checkpoints/longvu_qwen
-#python3 ./eval/eval_mvbench.py --data_path ./data/MVBench --version qwen --model_path ./checkpoints/longvu_qwen
